9b36b22a8348d17cddeff75b60ac184a31b98e3b
Miguel Mejía <mmejiabr@pichincha.com>
2021-11-19 11:34:50 -0500

    Markers in letters

--- src/main/java/com/pichincha/tenis/TenisApplication.java
+++ src/main/java/com/pichincha/tenis/TenisApplication.java
@@ -3,38 +3,50 @@ package com.pichincha.tenis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class TenisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TenisApplication.class, args);
-		int pointsPlayerOne = 0;
-		int pointsPlayerTwo = 0;
-	}
-	//Sumar puntos Rosemberth Bone.
-	private int addPoints(int pointsPlayerOne) {
-		return pointsPlayerOne + 1;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TenisApplication.class, args);
+        int pointsPlayerOne = 0;
+        int pointsPlayerTwo = 0;
+        pointsPlayerOne = addPoints(pointsPlayerOne);
 
-	private String displayResult(String playerName, int playerMarker) {
-		return playerName.concat(" ").concat(String.valueOf(playerMarker));
-	}
+        pointsPlayerTwo = addPoints(pointsPlayerTwo);
+        pointsPlayerTwo = addPoints(pointsPlayerTwo);
+        System.out.println(getMarker(pointsPlayerOne, pointsPlayerTwo));
+    }
 
-	private String getMarker(int pointsPlayerOne, int pointsPlayerTwo) {
-		if (pointsPlayerOne == pointsPlayerTwo) {
-			return "Deuce";
-		} else if (pointsPlayerOne > pointsPlayerTwo + 2) {
-			return "Player One Wins";
-		}  else if (pointsPlayerTwo > pointsPlayerOne + 2) {
-			return "Player Two Wins";
-		} else if (pointsPlayerOne == 4) {
-			return "Advantage Player One";
-		}  else if (pointsPlayerTwo == 4) {
-			return "Advantage Player Two";
-		}
-		return null;
-	}
+    //Sumar puntos Rosemberth Bone.
+    private static int addPoints(int pointsPlayerOne) {
+        return pointsPlayerOne + 1;
+    }
 
-	//Marcadores con un hashmap
+    private static String displayResult(String playerName, int playerMarker) {
+        Map<Integer, Integer> points = new HashMap<>();
+        points.put(0, 0);
+        points.put(1, 15);
+        points.put(2, 30);
+        points.put(3, 40);
+        return playerName.concat(" ").concat(String.valueOf(points.get(playerMarker)));
+    }
 
+    private static String getMarker(int pointsPlayerOne, int pointsPlayerTwo) {
+        if (pointsPlayerOne == pointsPlayerTwo) {
+            return "Deuce";
+        } else if (pointsPlayerOne > pointsPlayerTwo + 2) {
+            return "Player One Wins";
+        } else if (pointsPlayerTwo > pointsPlayerOne + 2) {
+            return "Player Two Wins";
+        } else if (pointsPlayerOne == 4) {
+            return "Advantage Player One";
+        } else if (pointsPlayerTwo == 4) {
+            return "Advantage Player Two";
+        } else {
+            return displayResult("Player One", pointsPlayerOne).concat(" ").concat(displayResult("Player Two", pointsPlayerTwo));
+        }
+    }
 }
