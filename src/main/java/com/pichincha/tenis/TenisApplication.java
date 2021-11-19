903c5c3c162c121d8de1da6adf437604887cdaae
Rosemberth Bone <rosemberth336@gmail.com>
2021-11-19 11:01:34 -0500

    Pair prigramming

--- src/main/java/com/pichincha/tenis/TenisApplication.java
+++ src/main/java/com/pichincha/tenis/TenisApplication.java
@@ -11,7 +11,7 @@ public class TenisApplication {
 		int pointsPlayerOne = 0;
 		int pointsPlayerTwo = 0;
 	}
-	//Sumar puntos sebas
+	//Sumar puntos Rosemberth Bone.
 	private int addPoints(int pointsPlayerOne) {
 		return pointsPlayerOne + 1;
 	}
